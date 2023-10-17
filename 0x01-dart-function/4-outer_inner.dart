25c269ac3be354a04bf8dde3f1e6febe09250bbf
chostler24 <connor.hostler@gmail.com>
2023-10-17 16:41:57 -0500

    pushing task 4 again

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,9 @@
 void outer(String name, String id) {
     String inner(String name, String id) {
-        return "Hello $name your id is $id";
+        List<String> names = name.split(" ");
+        String name0 = names[0];
+        String nameInitial = names[1][0];
+        return "Hello Agent $nameInitial.$name0 your id is $id";
     }
 
     print(inner(name, id));
