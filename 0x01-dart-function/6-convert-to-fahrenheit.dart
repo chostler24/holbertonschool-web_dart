ac31d0b0d35d2de1413e223a0eb33554bfb237e2
chostler24 <connor.hostler@gmail.com>
2023-10-17 16:53:21 -0500

    pushing task 6

---
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -0,0 +1,6 @@
+List<double> convertToF(List<double> temperaturesInC) {
+    var tempsInF = temperaturesInC.map(
+        (celsius) => double.parse(((celsius * 9 / 5) + 32).toStringAsFixed(2))
+    );
+    return tempsInF.toList();
+}
