6d08ce06b3797f988557ab7cb4dd3cecf53bd755
chostler24 <connor.hostler@gmail.com>
2023-10-18 17:32:11 -0500

    pushing task 3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,28 @@
+class Password {
+  String _password = '';
+
+  Password({required String password}) {
+    _password = password;
+  }
+
+    bool isValid() {
+        if (_password.length >= 8) {
+            if (_password.length <= 16) {
+                if (_password.contains(RegExp(r'[0-9]'))) {
+                    if (_password.contains(RegExp(r'[a-z]'))) {
+                        if (_password.contains(RegExp(r'[A-Z]'))) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @override
+    String toString() {
+        return "Your password is: $_password";
+    }
+}
